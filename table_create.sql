a132d470785905eb864245ec9d3dd9e8daff8a90
Rachel <rachel.fizel@gmail.com>
2021-05-02 21:14:01 -0500

    added connection to database and loaded 1 dataframe

---
+++ table_create.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "state_parks" (
+	"year" INT,
+	"state" varchar(2),
+	"park" varchar(50),
+	"park_visitor_count" INT,
+	"park_pct_change" varchar(10),
+	"state_visitor_count" INT,
+	"state_pct_change" varchar(10)
+);
+
+select * from state_parks;
