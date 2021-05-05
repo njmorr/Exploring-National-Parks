574a9663c814e4efdbcc421f8414a549ffeee706
Molly Sullivan <mchandler02@gmail.com>
2021-05-05 16:48:46 -0500

    created graphs.js file in static/js folder and started basic requirements for line graph

---
+++ static/js/graphs.js
@@ -0,0 +1,16 @@
+//Line graph for parks visitors
+var config = {
+    type: 'line',
+    data,
+    options:{}
+};
+
+//Need to add to html, but not sure where to put it
+{/* <script>
+  // === include 'setup' then 'config' above ===
+
+  var myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
+</script> */}
