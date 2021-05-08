bf5c2d6fd7431636ecf80ba6ef788af45dc91d76
Molly Sullivan <mchandler02@gmail.com>
2021-05-08 11:39:07 -0500

    created state_park table in postgres

--- table_create.sql
+++ table_create.sql
@@ -1,11 +1,19 @@
 CREATE TABLE "state_parks" (
-	"year" INT,
-	"state" varchar(20),
-	"park" varchar(50),
-	"park_visitor_count" INT,
-	"park_pct_change" varchar(10),
-	"state_visitor_count" INT,
-	"state_pct_change" varchar(10)
+	"PK" varchar PRIMARY KEY,
+	"State" varchar,
+	"Park" varchar,
+	"ParkVisitorCount_2016" FLOAT,
+	"ParkPctChange_2016" FLOAT,
+	"StateVisitorCount_2016" FLOAT,
+	"StatePctChange_2016" FLOAT,
+	"ParkVisitorCount_2015" FLOAT,
+	"ParkPctChange_2015" FLOAT,
+	"StateVisitorCount_2015" FLOAT,
+	"StatePctChange_2015" FLOAT,
+	"ParkVisitorCount_2014" FLOAT,
+	"ParkPctChange_2014" FLOAT,
+	"StateVisitorCount_2014" FLOAT,
+	"StatePctChange_2014" FLOAT
 );
 
 select * from state_parks;
